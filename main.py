d283cbe46e4cbaa2a110cbd435199f2ba9bd67a1
tsKIM <tay97kim@daegu.ac.kr>
2020-06-03 20:56:12 +0900

    출결정보와 lms에서 달력에 기입된 스케쥴을 읽어오는 코드를추가

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 import selenium.webdriver
 import selenium.webdriver
 import selenium.webdriver.support.ui
@@ -8,64 +9,106 @@ from selenium.webdriver.common.by import By
 import os
 import time
 from datetime import datetime, timedelta
+#options = Options()
+#options.add_argument('--headless')
+#options.add_argument('--no-sandbox')
 
 loginUrl = 'https://sso.daegu.ac.kr/dgusso/ext/lms/login_form.do?Return_Url=http://lms.daegu.ac.kr/ilos/lo/login_sso.acl'
+# https://sso.daegu.ac.kr/dgusso/ext/attend/login_form.do?Return_Url=http://attend.daegu.ac.kr > attendance
+# https://sso.daegu.ac.kr/dgusso/ext/lms/login_form.do?Return_Url=http://lms.daegu.ac.kr/ilos/lo/login_sso.acl > lms
 
-driver = selenium.webdriver.Chrome(os.path.dirname(os.path.realpath(__file__))+ '/chromedriver')
+driver = selenium.webdriver.Chrome()
 
 def login_service():
-    driver.get(loginUrl)
+	driver.get(loginUrl)
 
-    username = 'ENTER YOUR ID'
-    password = 'ENTER YOUR PASSWD'
+	username='21630209'
+	password='@@@k2i6m4t3s'
 
-    driver.find_element_by_id('usr_id').send_keys(username)
-    driver.find_element_by_id('usr_pw').send_keys(password)
+	driver.find_element_by_id('usr_id').send_keys(username)
+	driver.find_element_by_id('usr_pw').send_keys(password)
 
-    driver.find_element_by_class_name('btn_login').click()
+	driver.find_element_by_class_name('btn_login').click()
 
 def attendance_info():
-    driver.find_element_by_xpath('//*[@id="list_subject"]/tbody/tr[2]').click()
-    print(driver.find_element_by_xpath('//*[@id="course"]/li[1]/label').text + ' : ' + driver.find_element_by_xpath('//*[@id="course"]/li[1]/span').text)
-    print(driver.find_element_by_xpath('//*[@id="summary"]').text + '\n')
-    driver.find_element_by_xpath('//*[@id="menu"]/li[1]').click()
+	i = 2
+	attend = ""
+	try:
+		while(True):
+			j = str(i)
+			driver.find_element_by_xpath('//*[@id="list_subject"]/tbody/tr['+j+']').click()
+			
+			temp1 = driver.find_element_by_xpath('//*[@id="course"]/li[1]/label').text + ' : ' + driver.find_element_by_xpath('//*[@id="course"]/li[1]/span').text + '\n' ##
+			attend += temp1
+
+			temp2 = driver.find_element_by_xpath('//*[@id="summary"]').text + '\n'
+			attend += temp2+'\n'
+
+			driver.find_element_by_xpath('//*[@id="menu"]/li[1]').click()
+			i=i+1
+	except:
+		print('출석현황 조회완료')
+		eattend = attend.encode('utf-8')
+		file = open('/home/tay97kim/attendance_output_Kim.txt', 'w')
+
+		file.write(eattend)##
+		
+		file.close()
+		driver.quit
+    
 
 
 def find_schedule():
-    nowDate = datetime.now()
-    limit = 0 #일정 개수 카운트 변수
-    i=0
-    for i in range(0, 15):
-        setDate = (nowDate+timedelta(days=i))
-        getDate = setDate.strftime('%#m_%#d') #윈도우에서는 #로 0을 제거하지만, 우분투 환경에서는 #가 아닌 -를 통해 0 제거가 필요함
-        driver.find_element_by_id(getDate).click() #오늘날짜까지 출력 완료
-        print("["+getDate+"]") #날짜 출력
+	nowDate = datetime.now()
+	limit = 0 #일정 개수 카운트 변수
+	i=0
+	sche = ""
+	for i in range(0, 5):
+		setDate = (nowDate+timedelta(days=i))
+		getDate = setDate.strftime('%-m_%-d')
+		driver.find_element_by_id(getDate).click()
+		temp = getDate.split('_')
+		temp_cat = temp[0]+'월 '+temp[1]+'일'
+		print(temp_cat)
+		sche += temp_cat+'\n' #날짜 기록
+		print(sche)
         
-        try:
-            print(driver.find_element_by_xpath('//*[@id="shedule_calendar_form"]/div[2]').text) #일정 출력
-        except:
-            print("error") #일정이 없어도 예외는 발생 X (테스트용 구문)
-            continue
-        print("")
+		try:
+			sche += driver.find_element_by_xpath('//*[@id="shedule_calendar_form"]/div[2]').text+'\n' #해당일자 일정 기록
+		except:
+			continue
+		#print("")
+		sche += '\n'
+	esche = sche.encode('utf-8')
+	file = open('/home/tay97kim/schedule_output_Kim.txt', 'w')
 
-def my_course():
-    driver.find_element_by_class_name('icon-nm').click()
-    lecture_count = 2
-    while(True):
-        try:
-            count_str = str(lecture_count)
-            print(driver.find_element_by_xpath('//*[@id="lecture_list"]/div[1]/div[1]/div['+count_str+']').text)
-            lecture_count = lecture_count+1
-            print("")
-        except:
-            break #정규과목 존재하지 않을 때 종료
+	file.write(esche)
+		
+	file.close()
+	driver.quit
+
+'''def my_course():
+	driver.find_element_by_class_name('icon-nm').click()
+	lecture_count = 2
+	while(True):
+		try:
+			count_str = str(lecture_count)
+			print(driver.find_element_by_xpath('//*[@id="lecture_list"]/div[1]/div[1]/div['+count_str+']').text)
+			lecture_count = lecture_count+1
+			print("")
+		except:
+			break #정규과목 존재하지 않을 때 종료
+	driver.quit''' #미사용
     
         
 
 if __name__ == "__main__":
-    login_service()
-    time.sleep(1)
-    find_schedule()
-    time.sleep(1)
-    my_course()
-    time.sleep(1)
+
+
+	login_service()
+	time.sleep(1)
+	#attendance_info()
+	find_schedule()
+	#time.sleep(1)
+	#my_course()
+
