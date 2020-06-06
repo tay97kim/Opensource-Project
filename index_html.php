62d0d7b42a28623dfec85417c112fd34410ac26a
tsKIM <tay97kim@daegu.ac.kr>
2020-06-07 02:39:30 +0900

    웹 페이지 코드 최종 수정사항 반영

--- index_html.php
+++ index_html.php
@@ -1,147 +1,180 @@
-<!DOCTYPE html>
 <?
-exec("cd /opt/lampp/htdocs/ai && python3 test.py", $out, $status);
-$temp = $out[0]
+exec("cd /var/www/html/ai && python3 attendance_info_Baek.py", $out, $return);
+exec("cd /var/www/html/ai && python3 attendance_info_Kim.py", $out, $return);
+exec("cd /var/www/html/ai && python3 attendance_info_Sin.py", $out, $return);
+exec("cd /var/www/html/ai && python3 schedule_info_Baek.py", $out, $return);
+exec("cd /var/www/html/ai && python3 schedule_info_Kim.py", $out, $return);
+exec("cd /var/www/html/ai && python3 schedule_info_Sin.py", $out, $return);
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <title>Document</title>
-	<style type="text/css">
-	.cam-box{width : 320px; height : 380px; border : 1px solid; padding : 5px; margin : 10 10 10 10;}
-	</style>
-</head>
-<body> 
+        <style type="text/css">
+        .cam-box{width : 320px; height : 320px; border : 1px solid; padding : 5px; margin : 10 10 10 10;}
+    </style>
+    </head>
+<body>
     <div style="font-size:1.2em; color:green;"> <center>< DaeguUniv Automatic Login Service ></center></div>  <br>
     <center><div class="cam-box">
-	<div id="webcam-container"></div>
-	<div id="label-container"></div>
-	</div><center>
+        <div id="webcam-container"></div>
+        <div id="label-container"></div>
+        </div><center>
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js"></script>
     <script type="text/javascript">
-	
-	
-        // More API functions here:
-        // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
-    
-        // the link to your model provided by Teachable Machine export panel
         const URL = "./my_model/";
-    
         let model, webcam, labelContainer, maxPredictions;
-	var sName;
-        // Load the image model and setup the webcam
+        var sName;
+        var allText = {};
+        var person = {};
         async function init() {
             const modelURL = URL + "model.json";
             const metadataURL = URL + "metadata.json";
-    
-            // load the model and metadata
-            // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-            // or files from your local hard drive
-            // Note: the pose library adds "tmImage" object to your window (window.tmImage)
             model = await tmImage.load(modelURL, metadataURL);
             maxPredictions = model.getTotalClasses();
 
-            // Convenience function to setup a webcam
-            const flip = true; // whether to flip the webcam
-            webcam = new tmImage.Webcam(320, 320, flip); // width, height, flip
-            await webcam.setup(); // request access to the webcam
+            const flip = true;
+            webcam = new tmImage.Webcam(320, 320, flip);
+            await webcam.setup();
             await webcam.play();
             window.requestAnimationFrame(loop);
-    
-            // append elements to the DOM
+
             document.getElementById("webcam-container").appendChild(webcam.canvas);
             labelContainer = document.getElementById("label-container");
-            for (let i = 0; i < maxPredictions; i++) { // and class labels
+            for (let i = 0; i < maxPredictions; i++) {
                 labelContainer.appendChild(document.createElement("div"));
             }
         }
 
         async function loop() {
-	webcam.update(); // update the webcam frame
-//async function predict() {
-        // predict can take in an image, video or canvas html element
-		
-	const prediction = await model.predict(webcam.canvas);
+                webcam.update();
+                const prediction = await model.predict(webcam.canvas);
 
-        if(prediction[0].className == "BaekDH" && prediction[0].probability.toFixed(2) >= 0.95) {
-            labelContainer.childNodes[0].innerHTML = "BaekDH"
-		var result = confirm('baek?');
-		if (result == true ){
-		var test = '<?= $temp?>'
-		 labelContainer.childNodes[1].innerHTML = test
-		}
-		else{
-			webcam.update();
-			window.requestAnimationFrame(loop);
-		}
-			
-	} else if(prediction[1].className == "SinGS" && prediction[1].probability.toFixed(2) >= 0.95) {
-            labelContainer.childNodes[0].innerHTML = "SinGS"
-		confirm('Sin?');
-		if (result == true ){
-		var test = '<?= $temp?>'
-		 labelContainer.childNodes[1].innerHTML = test
-		}
-		else{
-			webcam.update();
-			window.requestAnimationFrame(loop);
-		}
-        } else if(prediction[2].className == "KimTs" && prediction[2].probability.toFixed(2) >= 0.95) {
-            labelContainer.childNodes[0].innerHTML = "KimTS"
-		confirm('kim?');
-		if (result == true ){
-		var test = '<?= $temp?>'
-		 labelContainer.childNodes[1].innerHTML = test
-		}
-		else{
-			webcam.update();
-			window.requestAnimationFrame(loop);
-		}
+            if(prediction[0].className == "BaekDH" && prediction[0].probability.toFixed(2) >= 0.90) {
+                var result1 = confirm('백대현 님이 맞습니까?');
+                if (result1 == true ){
+                    person.name = "백대현";
+                    document.getElementById('dialog-message').setAttribute('title', person.name + '>님이 로그인 되었습니다.');
+                    crayBtn1();
+                }
+                else{
+                    webcam.update();
+                    window.requestAnimationFrame(loop);
+                }
+
+            } else if(prediction[1].className == "SinGS" && prediction[1].probability.toFixed(2) >= 0.90) {
+                var result2 = confirm('신규섭 님이 맞습니까?');
+                if (result2 == true ){
+                    person.name = "신규섭";
+                    document.getElementById('dialog-message').setAttribute('title', person.name + '님이 로그인 되었습니다.');
+                    crayBtn2();
+                }
+                else{
+                    webcam.update();
+                    window.requestAnimationFrame(loop);
+                }
+            } else if(prediction[2].className == "KimTs" && prediction[2].probability.toFixed(2) >= 0.90) {
+                var result3 = confirm('김태성 님이 맞습니까?');
+                if (result3 == true ){
+                    person.name = "김태성";
+                    document.getElementById('dialog-message').setAttribute('title', person.name + '>님이 로그인 되었습니다.');
+                    crayBtn3();
+                }
+                else{
+                    webcam.update();
+                    window.requestAnimationFrame(loop);
+                }
+            }
+            else{
+                window.requestAnimationFrame(loop);
+            }
         }
-	 else{          
-	//await predict();
-            window.requestAnimationFrame(loop);
+        async function readTextFile(file)
+        {
+            var rawFile = new XMLHttpRequest();
+            rawFile.open("GET", file, false);
+            rawFile.onreadystatechange = function ()
+            {
+                if(rawFile.readyState === 4)
+                {
+                    if(rawFile.status === 200 || rawFile.status == 0)
+                    {
+                        allText.test = rawFile.responseText;
+                        //alert(allText);
+                    }
+                }
+            }
+            rawFile.send(null);
         }
 
-
-
-        // run the webcam image through the image model
-        /*async function predict() {
-        // predict can take in an image, video or canvas html element
-        const prediction = await model.predict(webcam.canvas);
-        if(prediction[0].className == "BaekDH" && prediction[0].probability.toFixed(2) >= 0.95) {
-            labelContainer.childNodes[0].innerHTML = "BaekDH"
-		setTimeout(function() {
-		confirm('baek?');
-		}, 3000);
-	} else if(prediction[1].className == "SinGS" && prediction[1].probability.toFixed(2) >= 0.95) {
-            labelContainer.childNodes[0].innerHTML = "SinGS"
-		setTimeout(function() {
-		confirm('Sin?');
-		}, 3000);
-        } else if(prediction[2].className == "KimTs" && prediction[2].probability.toFixed(2) >= 0.95) {
-            labelContainer.childNodes[0].innerHTML = "KimTS"
-		setTimeout(function() {
-		confirm('kim?');
-		}, 3000);
-        }
-	 else {
-		labelContainer.childNodes[0].innerHTML = "asd"
-		setTimeout(function() {
-		confirm('asd?');
-		}, 3000);
-	}*/
-        // for (let i = 0; i < maxPredictions; i++) {
-        //     const classPrediction =
-        //         prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-        //     labelContainer.childNodes[i].innerHTML = classPrediction;
-        // }
+    function crayBtn1() {
+        $('#dialog-message').dialog({
+            modal: true,
+            width : 330,
+            height: 200,
+            buttons: {
+                "출석정보": function() {
+                    readTextFile("attendance_output_Baek.txt");
+                    console.log('attendance');
+                    alert(allText.test);
+                },
+                "일정": function() {
+                    readTextFile("schedule_info_Baek.txt");
+                    console.log('schedule');
+                    alert(allText.test);
+                },
+                    "취소": function() {
+                    $(this).dialog('close');
+                }
+            }
+        });
     }
+    function crayBtn2() {
+        $('#dialog-message').dialog({
+            modal: true,
+            buttons: {
+                "출석정보": function() {
+                    readTextFile("attendance_output_Sin.txt");
+                    alert(allText.test);
+                },
+                "일정": function() {
+                    readTextFile("schedule_output_Sin.txt");
+                    alert(allText.test);
+                },
+                    "취소": function() {
+                    $(this).dialog('close');
+                }
+            }
+        });
+    }
+    function crayBtn3() {
+        $('#dialog-message').dialog({
+            modal: true,
+            buttons: {
+                "출석정보": function() {
+                    readTextFile("attendance_output_Kim.txt");
+                    alert(allText.test);
+                },
+                "일정": function() {
+                    readTextFile("schedule_output_Kim.txt");
+                    alert(allText.test);
+                },
+                "취소": function() {
+                    $(this).dialog('close');
+                }
+            }
+        });
+    }
+
     </script>
-	<center> <button type="button" style="padding:10px 150px;" onclick="init()">Start</button> </center>
+    <center> <button type="button" style="padding:10px 150px;" onclick="init()">Start</button> </center> <br/>
+    <div id="asd"></div>
   <!-- Footer -->
       <footer>
         <div class="container">
@@ -149,5 +182,7 @@ $temp = $out[0]
           </div>
         </div>
       </footer>
-    </div>
+
+    <div id="dialog-message" title="" style='display:none'>원하는 작업을 선택하세요.</div>
+
 </body>
